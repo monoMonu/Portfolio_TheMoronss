ea215a1763c262f4dd8232a236c318a11cc8d0d2
Ishaan Minocha <minochaishaan2003@gmail.com>
2024-11-15 20:52:25 +0530

    added tailwind + comp structure done

--- src/components/DetailsSec.jsx
+++ src/components/DetailsSec.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
+import DomainMethodologies from './DomainMethodologies'
 
 function DetailsSec() {
   return (
-    <div>
+    <div className='bg-[#f3f2ee] p-6'>
       <Navbar />
       DetailsSec
+      <DomainMethodologies />
    </div>
   )
 }
